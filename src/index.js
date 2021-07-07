f0f7df4d3e6fce8340b04b8ea9031d25f3d86720
Manko Ivan <inoi2002@gmail.com>
2021-07-08 00:16:38 +0300

    404 an 500 pages

--- src/index.js
+++ src/index.js
@@ -4,7 +4,17 @@ import { error } from "./pages/error/error";
 
 const root = document.querySelector("#root");
 
+const path = window.location.pathname;
 
+switch (path) {
+    case '/':
+        root.innerHTML = error({code: "Упс...", message: "Страницы пока нет, но уже скоро она появится"});
+        break;
+    case '/500':
+        root.innerHTML = error({code: "500", message: "Мы уже фиксим"});
+        break;
+    default:
+        root.innerHTML = error({code: "404", message: "Не туда попали"});
 
+} 
 
-root.innerHTML = error({code: "Упс...", message: "Страницы пока нет, но уже скоро она появится"});
